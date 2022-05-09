1bfda82d811c73de33148dbb8bed8686bffd03e8
Oleh Semennykov <sumcue@gmail.com>
2022-05-09 14:22:48 +0300

    add modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,45 +1,36 @@
-import React, { Component } from 'react';
-import {createPortal} from 'react-dom';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
 import s from './Modal.module.css';
 
-const modalRoot = document.querySelector('#modal-root')
-class Modal extends Component {
-  componentDidMount() {
-    console.log('componentDidMount');
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    console.log('componentWillUnmout');
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+const modalRoot = document.querySelector('#modal-root');
 
-  handleKeyDown = e => {
+export default function Modal({ children, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
       console.log('push the button escape');
-      this.props.onClose();
+      onClose();
     }
     console.log(e.code);
   };
 
-  handleBackdropClose = event => {
-    // console.log('кликнули в бэкдроппп');
-    // console.log('curentTarget', event.currentTarget);
-    // console.log('target', event.target);
+  const handleBackdropClose = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={s.overlay} onClick={this.handleBackdropClose}>
-        <div>{this.props.children}</div>
-      </div>,
-      modalRoot
-    );
-  }
-  //
-};
-
-export default Modal;
+  return createPortal(
+    <div className={s.overlay} onClick={handleBackdropClose}>
+      <div>{children}</div>
+    </div>,
+    modalRoot
+  );
+}
